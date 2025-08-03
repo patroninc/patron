a2ba1647af38b1df750909d4be67919404fe8308
Nick K <nick.k@trieve.ai>
2025-08-03 08:35:31 -0700

    cleanup: remove astrobits

--- astrobits/index.ts
+++
@@ -1,6 +0,0 @@
-import "../../styles/global.css";
-import Button from "./src/components/Button/Button.astro";
-import Card from "./src/components/Card/Card.astro";
-
-export { Button };
-export { Card };
