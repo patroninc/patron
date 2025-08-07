0fe800ea3bdec623f96f563b038bc01101bd997e
Nick K <nick.k@trieve.ai>
2025-08-07 00:42:10 -0700

    feat: add content and keystatic config for blog posts and categories

--- website/keystatic.config.ts
+++ website/keystatic.config.ts
@@ -3,6 +3,9 @@ import { testimonials } from "@lib/keystatic/collections/testimonials";
 import { homepage } from "@lib/keystatic/singletons/homepage";
 import { nav } from "@lib/keystatic/singletons/nav";
 import { footer } from "@lib/keystatic/singletons/footer";
+import { blog } from "@lib/keystatic/singletons/blog";
+import { blogCategories } from "@lib/keystatic/collections/blog-categories";
+import { blogPosts } from "@lib/keystatic/collections/blog-posts";
 
 export default config({
   storage: {
@@ -10,10 +13,13 @@ export default config({
   },
   collections: {
     testimonials,
+    blogCategories,
+    blogPosts,
   },
   singletons: {
     homepage,
     nav,
     footer,
+    blog,
   },
 });
--- website/src/content.config.ts
+++ website/src/content.config.ts
@@ -15,6 +15,38 @@ const testimonials = defineCollection({
     }),
 });
 
+const blogPosts = defineCollection({
+  loader: glob({
+    pattern: "**/*.mdx",
+    base: "./src/content/blog-posts",
+  }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      summary: z.string(),
+      author: z.string().optional(),
+      createdAt: z.date().nullish(),
+      lastUpdatedAt: z.date().nullish(),
+      ogSection: z.string().optional(),
+      isDraft: z.boolean(),
+      categories: z.array(z.string()),
+      coverImage: image(),
+    }),
+});
+
+const blogCategories = defineCollection({
+  loader: glob({
+    pattern: "**/*.yaml",
+    base: "./src/content/blog-categories",
+  }),
+  schema: () =>
+    z.object({
+      name: z.string(),
+    }),
+});
+
 export const collections = {
   testimonials,
+  blogPosts,
+  blogCategories,
 };
