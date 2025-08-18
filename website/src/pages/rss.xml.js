bd3fc5568b04327cec6e60cbe0344625a3be0df6
Nick K <nick.k@trieve.ai>
2025-08-18 00:26:17 -0700

    feat: add RSS feed

---
+++ website/src/pages/rss.xml.js
@@ -0,0 +1,27 @@
+import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
+import sanitizeHtml from "sanitize-html";
+import MarkdownIt from "markdown-it";
+const parser = new MarkdownIt();
+
+export async function GET(context) {
+  const blog = await getCollection("blogPosts");
+  return rss({
+    title: "Patron.com Blog",
+    description:
+      "The latest updates, insights, and stories from Patron - the creator-first platform empowering artists, writers, and creators with low fees and open-source transparency.",
+    site: context.site,
+    items: blog
+      .filter((post) => !(post.data.isDraft ?? false))
+      .sort((a, b) => new Date(b.data.date) - new Date(a.data.date))
+      .map((post) => ({
+        link: `/blog/${post.id}/`,
+        content: sanitizeHtml(parser.render(post.body), {
+          allowedTags: sanitizeHtml.defaults.allowedTags.concat(["img"]),
+        }),
+        pubDate: post.data.lastUpdatedAt,
+        ...post.data,
+      })),
+    customData: `<language>en-us</language>`,
+  });
+}
