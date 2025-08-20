34c3bebba6640848912cf179478d5e271b6ceef0
Samuel Breznjak <samuelbreznjak35@gmail.com>
2025-08-20 17:42:15 +0200

    feat: update ui components

--- client/app/components/ui/button.tsx
+++ client/app/components/ui/button.tsx
@@ -5,26 +5,18 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "~/lib/utils";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
+  "inline-flex items-center group peer relative justify-center gap-2 whitespace-nowrap text-sm transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none",
   {
     variants: {
       variant: {
-        default:
-          "bg-primary text-primary-foreground shadow-xs hover:bg-primary/90",
-        destructive:
-          "bg-destructive text-white shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
-        outline:
-          "border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50",
-        secondary:
-          "bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/80",
-        ghost:
-          "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
-        link: "text-primary underline-offset-4 hover:underline",
+        default: "bg-primary text-white",
+        destructive: "bg-destructive text-white",
+        secondary: "bg-secondary text-secondary-foreground",
       },
       size: {
         default: "h-9 px-4 py-2 has-[>svg]:px-3",
-        sm: "h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5",
-        lg: "h-10 rounded-md px-6 has-[>svg]:px-4",
+        sm: "h-8 gap-1.5 px-3 has-[>svg]:px-2.5",
+        lg: "h-10 px-6 has-[>svg]:px-4",
         icon: "size-9",
       },
     },
@@ -40,19 +32,60 @@ function Button({
   variant,
   size,
   asChild = false,
+  shadow = true,
   ...props
 }: React.ComponentProps<"button"> &
   VariantProps<typeof buttonVariants> & {
     asChild?: boolean;
+    shadow?: boolean;
   }) {
   const Comp = asChild ? RadixSlot.Slot : "button";
 
-  return (
+  const Btn = (
     <Comp
       data-slot="button"
-      className={cn(buttonVariants({ variant, size, className }))}
+      className={cn(
+        buttonVariants({ variant, size, className }),
+        shadow
+          ? "hover:translate-x-[3px] hover:translate-y-[3px] transition-transform relative z-[1] ease-in-out"
+          : "m-[3px]",
+      )}
       {...props}
-    />
+    >
+      <div className="absolute -top-[3px] left-[3px] w-[calc(100%-6px)] h-[3px] bg-black" />
+      <div className="absolute -bottom-[3px] left-[3px] w-[calc(100%-6px)] h-[3px] bg-black" />
+      <div className="absolute top-[3px] -left-[3px] h-[calc(100%-6px)] w-[3px] bg-black" />
+      <div className="absolute top-[3px] -right-[3px] h-[calc(100%-6px)] w-[3px] bg-black" />
+      <div className="absolute top-0 left-0 size-[3px] bg-black" />
+      <div className="absolute top-0 right-0 size-[3px] bg-black" />
+      <div className="absolute bottom-0 left-0 size-[3px] bg-black" />
+      <div className="absolute bottom-0 right-0 size-[3px] bg-black" />
+      {props.children}
+      <div className="absolute hidden group-focus-visible:block -top-[6px] left-[3px] w-[calc(100%-6px)] h-[3px] bg-white" />
+      <div className="absolute hidden group-focus-visible:block -bottom-[6px] left-[3px] w-[calc(100%-6px)] h-[3px] bg-white" />
+      <div className="absolute hidden group-focus-visible:block top-[3px] -left-[6px] h-[calc(100%-6px)] w-[3px] bg-white" />
+      <div className="absolute hidden group-focus-visible:block top-[3px] -right-[6px] h-[calc(100%-6px)] w-[3px] bg-white" />
+      <div className="absolute hidden group-focus-visible:block top-0 -left-[3px] size-[3px] bg-white" />
+      <div className="absolute hidden group-focus-visible:block -top-[3px] left-[0] size-[3px] bg-white" />
+      <div className="absolute hidden group-focus-visible:block top-0 -right-[3px] size-[3px] bg-white" />
+      <div className="absolute hidden group-focus-visible:block -top-[3px] right-[0] size-[3px] bg-white" />
+      <div className="absolute hidden group-focus-visible:block bottom-0 -left-[3px] size-[3px] bg-white" />
+      <div className="absolute hidden group-focus-visible:block -bottom-[3px] left-[0] size-[3px] bg-white" />
+      <div className="absolute hidden group-focus-visible:block bottom-0 -right-[3px] size-[3px] bg-white" />
+      <div className="absolute hidden group-focus-visible:block -bottom-[3px] right-[0] size-[3px] bg-white" />
+    </Comp>
+  );
+
+  return shadow ? (
+    <div className="p-[3px] relative">
+      {Btn}
+      <div className="absolute -bottom-[3px] left-[9px] w-[calc(100%-12px)] h-[3px] bg-black peer-disabled:bg-black/50" />
+      <div className="absolute top-[9px] -right-[3px] h-[calc(100%-12px)] w-[3px] bg-black peer-disabled:bg-black/50" />
+      <div className="absolute bottom-[0px] right-0 w-[3px] h-[6px] bg-black peer-disabled:bg-black/50" />
+      <div className="absolute bottom-[0px] right-[3px] w-[3px] h-[3px] bg-black peer-disabled:bg-black/50" />
+    </div>
+  ) : (
+    Btn
   );
 }
 
--- client/app/components/ui/input.tsx
+++ client/app/components/ui/input.tsx
@@ -4,17 +4,38 @@ import { cn } from "~/lib/utils";
 
 function Input({ className, type, ...props }: React.ComponentProps<"input">) {
   return (
-    <input
-      type={type}
-      data-slot="input"
-      className={cn(
-        "file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input flex h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
-        "focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]",
-        "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
-        className,
-      )}
-      {...props}
-    />
+    <div className="relative m-[3px]">
+      <input
+        type={type}
+        data-slot="input"
+        className={cn(
+          "file:text-foreground peer bg-white placeholder:text-muted-foreground selection:bg-primary selection:text-white flex h-9 w-full min-w-0 px-3 py-1 text-base transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+          "aria-invalid:ring-destructive/20 aria-invalid:border-destructive",
+          className,
+        )}
+        {...props}
+      />
+      <div className="absolute -top-[3px] left-[3px] w-[calc(100%-6px)] h-[3px] bg-black peer-[.has-error]:bg-destructive" />
+      <div className="absolute -bottom-[3px] left-[3px] w-[calc(100%-6px)] h-[3px] bg-black peer-[.has-error]:bg-destructive" />
+      <div className="absolute top-[3px] -left-[3px] h-[calc(100%-6px)] w-[3px] bg-black peer-[.has-error]:bg-destructive" />
+      <div className="absolute top-[3px] -right-[3px] h-[calc(100%-6px)] w-[3px] bg-black peer-[.has-error]:bg-destructive" />
+      <div className="absolute top-0 left-0 size-[3px] bg-black peer-[.has-error]:bg-destructive" />
+      <div className="absolute top-0 right-0 size-[3px] bg-black peer-[.has-error]:bg-destructive" />
+      <div className="absolute bottom-0 left-0 size-[3px] bg-black peer-[.has-error]:bg-destructive" />
+      <div className="absolute bottom-0 right-0 size-[3px] bg-black peer-[.has-error]:bg-destructive" />
+      <div className="absolute hidden peer-focus-visible:block -top-[6px] left-[3px] w-[calc(100%-6px)] h-[3px] bg-white" />
+      <div className="absolute hidden peer-focus-visible:block -bottom-[6px] left-[3px] w-[calc(100%-6px)] h-[3px] bg-white" />
+      <div className="absolute hidden peer-focus-visible:block top-[3px] -left-[6px] h-[calc(100%-6px)] w-[3px] bg-white" />
+      <div className="absolute hidden peer-focus-visible:block top-[3px] -right-[6px] h-[calc(100%-6px)] w-[3px] bg-white" />
+      <div className="absolute hidden peer-focus-visible:block top-0 -left-[3px] size-[3px] bg-white" />
+      <div className="absolute hidden peer-focus-visible:block -top-[3px] left-[0] size-[3px] bg-white" />
+      <div className="absolute hidden peer-focus-visible:block top-0 -right-[3px] size-[3px] bg-white" />
+      <div className="absolute hidden peer-focus-visible:block -top-[3px] right-[0] size-[3px] bg-white" />
+      <div className="absolute hidden peer-focus-visible:block bottom-0 -left-[3px] size-[3px] bg-white" />
+      <div className="absolute hidden peer-focus-visible:block -bottom-[3px] left-[0] size-[3px] bg-white" />
+      <div className="absolute hidden peer-focus-visible:block bottom-0 -right-[3px] size-[3px] bg-white" />
+      <div className="absolute hidden peer-focus-visible:block -bottom-[3px] right-[0] size-[3px] bg-white" />
+    </div>
   );
 }
 
