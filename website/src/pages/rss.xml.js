e6dc76254c7a620206c8faa67523944ad99a4d2f
Nick K <nick.k@trieve.ai>
2025-09-02 06:27:54 -0700

    feat: add new website blog about building SSR with Vite

--- website/src/pages/rss.xml.js
+++ website/src/pages/rss.xml.js
@@ -12,7 +12,11 @@ export async function GET(context) {
       "The latest updates, insights, and stories from Patron - the creator-first platform empowering artists, writers, and creators with low fees and open-source transparency.",
     site: context.site,
     items: blog
-      .filter((post) => !(post.data.isDraft ?? false) && !post.data.categories?.includes("changelog"))
+      .filter(
+        (post) =>
+          !(post.data.isDraft ?? false) &&
+          !post.data.categories?.includes("changelog"),
+      )
       .sort((a, b) => new Date(b.data.date) - new Date(a.data.date))
       .map((post) => ({
         link: `/blog/${post.id}/`,
