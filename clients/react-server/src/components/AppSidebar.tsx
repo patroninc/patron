216250573f9a39660796e3a24f6873fbe5841ff1
Samuel Breznjak <samuelbreznjak35@gmail.com>
2025-09-06 01:09:38 +0200

    feat: add sidebar component

---
+++ clients/react-server/src/components/AppSidebar.tsx
@@ -0,0 +1,95 @@
+import { User, LayoutDashboard, Library, ChartBar, Users, DollarSign } from "lucide-react"
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "../components/ui/sidebar"
+
+const items = [
+  {
+    title: "Dashboard",
+    url: "/",
+    icon: LayoutDashboard,
+  },
+  {
+    title: "Content",
+    url: "/content",
+    icon: Library,
+  },
+  {
+    title: "Insights",
+    url: "/insights",
+    icon: ChartBar,
+  },
+  {
+    title: "Audience",
+    url: "/audience",
+    icon: Users,
+  },
+  {
+    title: "Payouts",
+    url: "/payouts",
+    icon: DollarSign,
+  },
+]
+
+export function AppSidebar() {
+  return (
+    <Sidebar collapsible="icon">
+      <SidebarHeader>
+        <SidebarMenu className="flex">
+          <SidebarMenuItem className="w-max">
+            <SidebarMenuButton className="p-0 w-max rounded-none" size="icon">
+              <img src="/logo.svg" alt="logo" className="size-8" />
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => {
+                const IconComponent = item.icon
+                return (
+                  <SidebarMenuItem key={item.title}>
+                    <SidebarMenuButton asChild>
+                      <a href={item.url}>
+                        <IconComponent />
+                        <span>{item.title}</span>
+                      </a>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                )
+              })}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+      
+      <SidebarFooter>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton size="lg">
+              <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
+                <User className="size-4" />
+              </div>
+              <div className="grid flex-1 text-left text-sm leading-tight">
+                <span className="truncate font-semibold">Username</span>
+                <span className="truncate text-xs">username@example.com</span>
+              </div>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarFooter>
+    </Sidebar>
+  )
+}
