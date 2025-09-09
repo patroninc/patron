b2233cfc8f43f71f891befee657901ca0dbaa501
Samuel Breznjak <samuelbreznjak35@gmail.com>
2025-09-09 02:37:42 +0200

    feat: add banner warning for unverified email

--- clients/react-server/src/layouts/main.tsx
+++ clients/react-server/src/layouts/main.tsx
@@ -1,6 +1,8 @@
 import { JSX } from 'react';
 import { SidebarProvider, SidebarTrigger, SidebarInset } from '../components/ui/sidebar';
 import { AppSidebar } from '../components/AppSidebar';
+import { useAuth } from '../contexts/AuthContext';
+import { Button } from '../components/ui/button';
 
 /**
  *
@@ -9,17 +11,31 @@ import { AppSidebar } from '../components/AppSidebar';
  * @returns {React.ReactElement} The Layout component
  */
 const MainLayout = ({ children }: { children: JSX.Element }): JSX.Element => {
+  const { user } = useAuth();
+
   return (
     <SidebarProvider>
       <AppSidebar />
       <SidebarInset>
+        <div className="flex flex-col">
+          {!user?.emailVerified && (
+            <div className="bg-warning flex items-center justify-between gap-4 border-b-5 border-b-black px-6 py-3">
+              <p className="text-lg">
+                Your email is not verified, check the inbox for confirmation email.
+              </p>
+              <Button shadow={false} variant="secondary" containerClassName="w-max">
+                Resend confirmation email
+              </Button>
+            </div>
+          )}
+          {children}
+        </div>
         <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
           <SidebarTrigger className="-ml-1" />
           <div className="flex items-center gap-2">
             <h1 className="text-lg font-semibold">Dashboard</h1>
           </div>
         </header>
-        <div className="flex flex-1 flex-col gap-4 p-4">{children}</div>
       </SidebarInset>
     </SidebarProvider>
   );
